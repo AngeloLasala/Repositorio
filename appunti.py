e23943f477a7ae9b657f541aaf88c4f0d9352918
AngeloLasala <angelo.lasala96@gmail.com>
2020-10-02 12:26:39 +0200

    cambiato appuinti e elimitato nuovo

--- appunti.py
+++ appunti.py
@@ -2,6 +2,7 @@
 
 # git branch new_stuff       (nome del nuovo file)  non si vede ancora su github
 # git checkout new_stuff     mi fa passare sul nuovo branch
+# git branch -d new_staff     elimina il branch locale e lo puoi fare solo se sei sul master 
 
 # come inserirli nel master merge: attraverso una pull request
 
