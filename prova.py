edccd1fcb1aa140c76fb94caf2f2c1cb80e2f28d
AngeloLasala <angelo.lasala96@gmail.com>
2020-10-05 17:43:13 +0200

    some stuff

--- prova.py
+++ prova.py
@@ -4,6 +4,12 @@ Created on Wed Sep 30 23:23:53 2020
 
 @author: Angelo
 """
+
+import argparse  #modulo che permette di dare comandi sulla linea di comando
+if __name__=="__main__":
+    parser=argparse.ArgumentParser()
+
+
 a={'a':0}
 t=(3.,5.)
 print(type(t[0]))
