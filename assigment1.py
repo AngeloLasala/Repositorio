9c7269a8f9b370fe00c9b0d73741cde11d8b112d
AngeloLasala <angelo.lasala96@gmail.com>
2020-10-05 16:04:03 +0200

    primo import

--- assigment1.py
+++ assigment1.py
@@ -4,14 +4,14 @@ imput_book=input('Enter the path of book:')
 f=open(imput_book,'r')
 book=f.read()
 
-alphabet = {('a','A'): 0, ('b'): 0, ('c'): 0, ('d'): 0}
+alphabet = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+alp =[['a','A'],['b','B'],['c','C'],['d','D']]
 
-for elements in alphabet:
-    print(type(elements))
-    for letters in book:
-        if letters==elements :
-            alphabet[elements]+=1
-    print(elements,alphabet[elements])
+for lett_alp in alp:
+    for lett_book in book:
+        if lett_book==lett_alp[0] or lett_book==lett_alp[1]:
+            alphabet[lett_alp[0]]+=1
+    print(lett_alp[0],alphabet[lett_alp[0]])
 
 
 
