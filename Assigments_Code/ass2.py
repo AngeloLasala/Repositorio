301f00018305fe7373cadf09b2e17be232012c0d
AngeloLasala <angelo.lasala96@gmail.com>
2021-01-25 12:47:39 +0100

    initial import assi2

--- Assigments_Code/ass2.py
+++ Assigments_Code/ass2.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 from matplotlib import pyplot as plt
 from scipy import interpolate
 
@@ -6,34 +6,54 @@ from scipy import interpolate
 class VoltageData:
     """Class for handling a set of measurements of the voltage at different
     times.""" 
-    pass
+    
+    def __init__(self, times, voltages):
+        t = np.array(times, dtype=np.float64)
+        v = np.array(voltages, dtype=np.float64)
+        self._data = np.column_stack((t,v))
 
+    @property
+    def voltages(self):
+        return self._data[:,1]
+
+    @property
+    def timestamps(self):
+        return self._data[:,0]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __getitem__(self,index):  #index qualsiasi cosa, posso fare anche lo slice
+        return self._data[index]
+
+    def __str__(self):
+        output_string= list()
+        for i, element in enumerate(self._data):
+            output_string.append(f't={element[0]} v={element[1]}')
+        return '\n'.join(output_string)
 
 if __name__ == '__main__':
     """ Here we test the functionalities of our class. These are not proper
     UnitTest - which you will se in a future lesson."""
     # Load some data
-    t, v = numpy.loadtxt('sandbox/sample_data_file.txt', unpack=True)
+    t, v = np.loadtxt('C:/Users/lasal/Desktop/temp/Repository_CMFEFDA_20-21/sample_voltage_data.txt', unpack=True)
     # Thest the constructor
     v_data = VoltageData(t, v)
     # Test len()
     assert len(v_data) == len(t)
     # Test the timestamps attribute
-    assert numpy.all(v_data.voltages == v)
+    assert np.all(v_data.voltages == v)
     # Test the voltages attribute
-    assert numpy.all(v_data.timestamps == t)
+    assert np.all(v_data.timestamps == t)
     # Test square parenthesis
     assert v_data[3, 1] == v[3]
     assert v_data[-1, 0] == t[-1]
     # Test slicing
-    assert numpy.all(v_data[1:5, 1] == v[1:5])
+    assert np.all(v_data[1:5, 1] == v[1:5])
     # Test iteration
     for i, entry in enumerate(v_data):
         assert entry[0] == t[i]
         assert entry[1] == v[i]
     # Test printing
     print(v_data)
-    # Test plotting
-    plt.figure('Voltage vs Time')
-    v_data.plot(plt.gca(), fmt='r+')
-    plt.show()
+    
